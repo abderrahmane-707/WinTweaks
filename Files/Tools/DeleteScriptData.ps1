4628abb8a1dd254aaf3ad8fdf7e8402236f04b02
abderrahmane <k.abderrahmane707@gmail.com>
2026-06-22 15:17:08 +0100

    style(DeleteScriptData)

--- Files/Tools/DeleteScriptData.ps1
+++ Files/Tools/DeleteScriptData.ps1
@@ -12,12 +12,11 @@ if (Test-Path $path) {
     Write-Host " Files: $count"
 
     # Request confirmation for deletion
-    Write-Host ""
-    choice /C YN /N /M "Delete all data and backups files in Win_Tweaks folder? (Y/N): "
+    choice /C YN /N /M "`nDelete all data and backups files in WinTweaks folder? (Y/N): "
     
     if ($LASTEXITCODE -eq 1) {
         Remove-Item -Path $path -Recurse -Force
-        Write-Host " All data has been deleted"
+        Write-Host "All data has been deleted"
     }
 
 } else {
