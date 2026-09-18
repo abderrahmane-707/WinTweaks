a24cd74b67798a068b5e3ad6af2a0dabbbeeb0e4
abderrahmane <k.abderrahmane707@gmail.com>
2026-09-18 20:16:28 +0100

    feat(DNSStatus)

--- Files/Network/DNSStatus.ps1
+++ Files/Network/DNSStatus.ps1
@@ -1,7 +1,17 @@
 # Show DNS servers configured on active network adapters
 Write-Host "Network Adapters DNS Settings:"
 
-$net = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True"
+# Get the interface indexes that are actually active (Status = Up)
+$activeIndexes = (Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }).ifIndex
+
+$net = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter "IPEnabled = True" |
+    Where-Object { $_.InterfaceIndex -in $activeIndexes }
+
+# Check for active adapters before proceeding
+if (-not $net) {
+    Write-Host "There are currently no enabled network adapters"
+    return
+}
 
 foreach ($n in $net) {
     Write-Host "`nAdapter: $($n.Description)"
@@ -9,7 +19,9 @@ foreach ($n in $net) {
     if ($n.DNSServerSearchOrder) {
         $dnsCount = 1
         foreach ($dns in $n.DNSServerSearchOrder) {
-            Write-Host "  DNS Server ${dnsCount}: $dns"
+            # Determine the address type: IPv4 or IPv6
+            $type = if ($dns -match ':') { "IPv6" } else { "IPv4" }
+            Write-Host "  DNS Server ${dnsCount} (${type}): $dns"
             $dnsCount++
         }
     }
