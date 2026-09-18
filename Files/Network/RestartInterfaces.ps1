c653950ae3b6ef476ed1bc1fca58668a65b155d7
abderrahmane <k.abderrahmane707@gmail.com>
2026-09-18 20:12:11 +0100

    refactor(RestartInterfaces)

--- Files/Network/RestartInterfaces.ps1
+++ Files/Network/RestartInterfaces.ps1
@@ -5,10 +5,19 @@ param (
 
 . "$PSScriptRoot\..\Common\Logger.ps1"
 
-Get-NetAdapter | Where-Object { $_.Status -eq 'Up' } | ForEach-Object {
+$activeInterfaces = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
+
+# Check for active interfaces before proceeding
+if (-not $activeInterfaces) {
+    Write-Log "There are currently no active network interfaces"
+    return
+}
+
+$activeInterfaces | ForEach-Object {
     try {
         Write-Log " - Restarting adapter: $($_.Name)"
-        Restart-NetAdapter -Name $_.Name -Confirm:$false -ErrorAction Stop
+        # Use InterfaceDescription instead of Name because it is more stable
+        Restart-NetAdapter -InterfaceDescription $_.InterfaceDescription -Confirm:$false -ErrorAction Stop
     }
     catch {
         Write-Log "   Failed to restart $($_.Name): $($_.Exception.Message)"
